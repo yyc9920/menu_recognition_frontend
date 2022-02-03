e7b8cd1261a1ccf940624f94f62b0e1c6a23fd91
Kendrick Yun <yyc837021@gmail.com>
2022-02-03 15:51:34 +0900

    Refactored Restaurant Content Box

--- src/RestaurantItem.js
+++ src/RestaurantItem.js
@@ -1,20 +1,10 @@
 import * as React from 'react';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 
+import ContentBox from './ContentBox'
 import restaurants from './RestaurantList';
 import './RestaurantItem.css';
-
-function NewlineText(props) {
-    const text = props.text;
-    return <div className="newlineText">{text}</div>;
-}
 
 function RestaurantItem() {
     const [restaurant, setRestaurant] = React.useState([]);
@@ -28,32 +18,7 @@ function RestaurantItem() {
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                 <Divider variant="inset" component="li" />
                 {restaurant.map((res) => (
-                    <div className="restaurant" key={res.name}>
-                        <ListItem alignItems="flex-start">
-                            <ListItemButton>
-                                <ListItemAvatar>
-                                    <Avatar alt={res.name} src={res.imgUrl} />
-                                </ListItemAvatar>
-                                <ListItemText
-                                    primary={res.name}
-                                    secondary={
-                                        <React.Fragment>
-                                            <Typography
-                                                sx={{ display: 'inline' }}
-                                                component="span"
-                                                variant="body2"
-                                                color="text.primary"
-                                            >
-                                                {res.category.join(', ')}
-                                            </Typography>
-                                            <NewlineText text={` — ${res.menus.join(', ')} 등\n${res.recent}  ⭐${res.rate}`} />
-                                        </React.Fragment>
-                                    }
-                                />
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-                    </div>
+                    <ContentBox restaurant={res} />
                 ))}
             </List>
         </div>
